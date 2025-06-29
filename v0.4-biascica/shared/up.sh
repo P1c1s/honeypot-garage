51aabb577c80b9b99a350a4c5abbfc99fdcdf130
Michela Sicuranza <166598112+SicMic@users.noreply.github.com>
2025-06-30 01:21:58 +0200

    iptables

--- v0.4-biascica/shared/up.sh
+++ v0.4-biascica/shared/up.sh
@@ -5,3 +5,5 @@ echo "192.169.0.2 mdb" >> /etc/hosts
 echo "192.169.0.3 ws1a" >> /etc/hosts
 echo "192.169.0.4 smb" >> /etc/hosts
 echo "192.168.0.2 sshserver" >> /etc/hosts
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "alias ipt='/shared/./iptables.sh'" >> /root/.bashrc
